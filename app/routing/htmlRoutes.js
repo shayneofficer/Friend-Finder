6cc6367b69d45c7a58033abcd2196f4c52411bf0
shayneofficer <shayneofficer@gmail.com>
2018-12-22 22:27:54 -0600

    made home route catch all / default route

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -7,13 +7,11 @@ router.use(express.json());
 
 // A GET Route to /survey which should display the survey page.
 router.get("/survey", function (req, res) {
-    console.log(path.join(__dirname, "../public/survey.html"));
-
     res.sendFile(path.join(__dirname, "../public/survey.html"));
 });
 
 // A default, catch-all route that leads to home.html which displays the home page.
-router.get("/", function (req, res) {
+router.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
 });
 
