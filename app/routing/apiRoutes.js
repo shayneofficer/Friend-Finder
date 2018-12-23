aac279ac9997a164055c4803c2e0992d6815d7e6
shayneofficer <shayneofficer@gmail.com>
2018-12-23 00:26:18 -0600

    api routes working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,8 @@
 var mysql = require("mysql");
 
+var express = require("express");
+var router = express.Router();
+
 var connection;
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
@@ -16,5 +19,70 @@ if (process.env.JAWSDB_URL) {
 connection.connect(function (err) {
     if (err) {
         console.error("error connecting: " + err.stack);
+        return;
     }
+
+    console.log("connected as id " + connection.threadId);
 });
+
+router.get("/api/friends", function (req, res) {
+
+    connection.query("SELECT * FROM profiles", function (err, results) {
+        res.json(results);
+    })
+});
+
+router.post("/api/friends", function (req, res) {
+
+    var scoresString = "[";
+    for (var i = 0; i < req.body.scores.length - 1; i++) {
+        scoresString += req.body.scores[i] + ", ";
+    }
+    scoresString += req.body.scores[req.body.scores.length - 1] + "]"
+
+    connection.query("INSERT INTO profiles SET ?",
+        {
+            name: req.body.name,
+            photo: req.body.photo,
+            scores: scoresString
+        },
+        function (err, results) {
+            if (err) throw err;
+            console.log("New profile added!");
+        }
+    );
+
+    connection.query(
+        "SELECT * FROM profiles",
+        function (err, results) {
+            if (err) throw err;
+
+            var data = JSON.stringify(results);
+            friends = JSON.parse(data);
+
+            var currentUser = friends[friends.length - 1];
+            var minDifference = 41;
+            var friendIndex;
+
+            for (var i = 0; i < friends.length - 1; i++) {
+                var diff = compareScores(currentUser, friends[i]);
+                if (diff < minDifference) {
+                    minDifference = diff;
+                    friendIndex = i;
+                }
+            }
+            res.json(friends[friendIndex]);
+        });
+});
+
+function compareScores(user, friend) {
+    userScores = JSON.parse(user.scores);
+    friendScores = JSON.parse(friend.scores);
+    var totalDiff = 0;
+    for (var i = 0; i < userScores.length; i++) {
+        totalDiff += Math.abs(userScores[i] - friendScores[i]);
+    }
+    return totalDiff;
+}
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,11 +6,11 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+var apiRoutes = require("./app/routing/apiRoutes");
+app.use(apiRoutes);
+
 var htmlRoutes = require("./app/routing/htmlRoutes");
 app.use(htmlRoutes);
-
-// var apiRoutes = require("./app/routing/apiRoutes");
-// app.use(apiRoutes);
 
 app.listen(PORT, function () {
     console.log("Listening on port " + PORT);
